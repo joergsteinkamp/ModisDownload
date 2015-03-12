24bfc95e9a658b7ecbe4960eee9dcca30662bd99
Joerg Steinkamp <joerg.steinkamp@senckenberg.de>
2015-03-12 13:14:56 +0100

    Now with an internal download function: modisDownload

--- lib/ModisDownload.pm
+++ lib/ModisDownload.pm
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 use LWP::UserAgent;
 use File::Path qw(make_path);
+use File::Basename;
 use Date::Simple;
 use List::Util qw(any min max);
 
@@ -20,7 +21,7 @@ our @ISA = qw(Exporter);
 # will save memory.
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-our @EXPORT = qw(loadCache initCache refreshCache getVersions getURLs isGlobal);
+our @EXPORT = qw(loadCache initCache refreshCache getVersions getURLs modisDownload isGlobal);
 
 our $VERSION = '0.01';
 
@@ -45,7 +46,7 @@ if ($^O eq "linux") {
   die("OS \"$^O\" not implemented!\n");
 }
 
-########################################################################
+#######################################################################
 ### For debuging and informations
 
 # returns the cached products data hash
@@ -99,7 +100,7 @@ sub getDateFullURLs($$) {
 
   my $ua = new LWP::UserAgent;
   my $response = $ua->get("${BASE_URL}/$MODIS{$prod}/$prod/$date");
-
+  
   unless ($response->is_success) {
    die $response->status_line;
   }
@@ -124,6 +125,11 @@ sub isGlobal($) {
   my $prod = shift;
   my $retval = 1;
 
+  if (! any { /$prod/ } keys %MODIS) {
+    warn("\"$prod\" is not in the MODIS product list: Check name or refresh the cache.\n");
+    return(-1);
+  }
+  
   my @flist = getDateFullURLs($prod, ${$MODISDates{$prod}}[0]);
   my $teststr = $flist[0];
   $teststr =~ s/.*\///;
@@ -225,7 +231,7 @@ sub setCache ($) {
   }
 
   if ( ! -d $CacheDir ) {
-    make_path($CacheDir) || warn("Could not create cache dir ('$CacheDir'): $!\n");
+    make_path($CacheDir) or warn("Could not create cache dir ('$CacheDir'): $!\n");
   }
 
   my $fhd;
@@ -234,30 +240,30 @@ sub setCache ($) {
   my %lookupTable = getAvailData();
   my $N = keys(%lookupTable);
 
-  open($fhd, ">", "$CacheDir/$ModisCacheFile") || 
+  open($fhd, ">", "$CacheDir/$ModisCacheFile") or 
     warn("cannot open \"$CacheDir/$ModisCacheFile\" for writing: $!\n");
 
   for (keys %lookupTable) {
     print  $fhd "'$_' => '$lookupTable{$_}',\n";
   }
-  close($fhd) || warn "close \"$CacheDir/$ModisCacheFile\" failed: $!";
+  close($fhd) or warn "close \"$CacheDir/$ModisCacheFile\" failed: $!";
   %MODIS = do "$CacheDir/$ModisCacheFile";
 
   ### Set up the MODIS dates cache
   %lookupTable = getAvailDates(\%MODIS);
   $N = keys(%lookupTable);
 
-  open($fhd, ">", "$CacheDir/$ModisDatesCacheFile") ||
+  open($fhd, ">", "$CacheDir/$ModisDatesCacheFile") or
     warn("cannot open \"$CacheDir/$ModisDatesCacheFile\" for writing: $!\n");
 
   for (keys %lookupTable) {
     print  $fhd "'$_' => $lookupTable{$_},\n";
   }
-  close($fhd) || warn "close \"$CacheDir/$ModisDatesCacheFile\" failed: $!";
+  close($fhd) or warn "close \"$CacheDir/$ModisDatesCacheFile\" failed: $!";
   %MODISDates = do "$CacheDir/$ModisDatesCacheFile";
 
   ### Set up the MODIS global check cache
-  open($fhd, ">", "$CacheDir/$ModisGlobalCacheFile") ||
+  open($fhd, ">", "$CacheDir/$ModisGlobalCacheFile") or
     warn("cannot open \"$CacheDir/$ModisGlobalCacheFile\" for writing: $!\n");
 
   for (keys %MODIS) {
@@ -315,39 +321,65 @@ sub createDownloadURLs(@) {
 
     my @fullURLs = getDateFullURLs($prod, $date->format("%Y.%m.%d"));
 
-    foreach my $i (min(@h)..max(@h)) {
-      foreach my $j (min(@v)..max(@v)) {
-        my $pat = sprintf("h%02iv%02i", $i, $j);
-        my @newURLs = ();
-        foreach (@fullURLs) {
-          if (/$pat/) {
-            push(@newURLs, $_);
+    if (!$MODISGlobal{$prod}) {
+      foreach my $i (min(@h)..max(@h)) {
+        foreach my $j (min(@v)..max(@v)) {
+          my $pat = sprintf("h%02iv%02i", $i, $j);
+          my @newURLs = ();
+          foreach (@fullURLs) {
+            if (/$pat/) {
+              push(@newURLs, $_);
+            }
           }
-        }
 
-        my $nNewURLs = @newURLs;
-        if ($nNewURLs == 1) {
-          push(@URLs, $newURLs[0]);
-        } elsif ($nNewURLs < 1) {
-          warn(sprintf("$prod: Missing file for %s @ %s.\n", $pat, $date->format("%Y.%m.%d")));
-        } else { 
+          my $nNewURLs = @newURLs;
+          if ($nNewURLs == 1) {
+            push(@URLs, $newURLs[0]);
+          } elsif ($nNewURLs < 1) {
+            warn(sprintf("$prod: Missing file for %s @ %s.\n", $pat, $date->format("%Y.%m.%d")));
+          } else { 
           # check for duplicate files here and choose the latest one
-          warn(sprintf("$prod: %i files for %s @ %s, choosing the newest.\n", $nNewURLs, $pat, $date->format("%Y.%m.%d")));
-          my $createDate = $newURLs[0];
-          $createDate =~ s/\.hdf$//;
-          $createDate =~ s/^.*\.//g;
-          $createDate = int($createDate);
-          my $newest = 0;
-          for (my $k=0; $k < $nNewURLs; $k++) {
-            s/\.hdf$//;
-            s/^.*\.//g;
-            if (int($_) > $createDate) {
-              $newest = $k;
-              $createDate = int($_);
-            }  
+            warn(sprintf("$prod: %i files for %s @ %s, choosing the newest.\n", $nNewURLs, $pat, $date->format("%Y.%m.%d")));
+            my $createDate = $newURLs[0];
+            $createDate =~ s/\.hdf$//;
+            $createDate =~ s/^.*\.//g;
+            $createDate = int($createDate);
+            my $newest = 0;
+            for (my $k=0; $k < $nNewURLs; $k++) {
+              s/\.hdf$//;
+              s/^.*\.//g;
+              if (int($_) > $createDate) {
+                $newest = $k;
+                $createDate = int($_);
+              }  
+            }
+            push(@URLs, $newURLs[$newest]);
           }
-          push(@URLs, $newURLs[$newest]);
         }
+      }
+    } else {
+      my $nURLs = @fullURLs;
+      if ($nURLs == 1) {
+        push(@URLs, $fullURLs[0]);
+      } elsif ($nURLs < 1) {
+        warn(sprintf("$prod: Missing file @ %s.\n", $date->format("%Y.%m.%d")));
+      } else { 
+      # check for duplicate files here and choose the latest one
+        warn(sprintf("$prod: %i files @ %s, choosing the newest.\n", $nURLs, $date->format("%Y.%m.%d")));
+        my $createDate = $fullURLs[0];
+        $createDate =~ s/\.hdf$//;
+        $createDate =~ s/^.*\.//g;
+        $createDate = int($createDate);
+        my $newest = 0;
+        for (my $k=0; $k < $nURLs; $k++) {
+          s/\.hdf$//;
+          s/^.*\.//g;
+          if (int($_) > $createDate) {
+            $newest = $k;
+            $createDate = int($_);
+          }  
+        }
+        push(@URLs, $fullURLs[$newest]);
       }
     }
   }
@@ -355,6 +387,8 @@ sub createDownloadURLs(@) {
 }
 
 ########################################################################
+### Function returning the URL file list
+
 sub getURLs(@) {
   my $nArgs = @_;
     
@@ -408,7 +442,121 @@ sub getURLs(@) {
 }
 
 ########################################################################
-### Exported functions
+### Function for download
+sub modisDownload (@) {
+  my $nArgs = @_;
+  my $refURL = shift;
+  my $success = 0;
+  $success=1 if ("$refURL" ne "");
+  if (!$success) {
+    warn("No URL(s) given!\n");
+    return(-1);
+  }
+  my $refDestination;
+  my $forceReload = 0;
+  my $verbose = 0;
+  
+  if ($nArgs == 1) {
+    $refDestination = ".";
+  } else {
+    $refDestination = shift;
+  }
+
+  $verbose = 1 if ($nArgs > 2);
+  $forceReload = 1 if ($nArgs > 3);
+
+  my @URL = ();
+  my @destination = ();
+
+    # check if these are a reference or a single file first
+  if ($refURL =~ /^ARRAY\(0x[a-z0-9]{7}\)$/) {
+    @URL = @$refURL;
+  } else {
+    $URL[0] = $refURL;
+  }
+  if ($refDestination =~ /^ARRAY\(0x[a-z0-9]{7}\)$/) {
+    @destination = @$refDestination;
+  } else {
+    $destination[0] = $refDestination;
+  }
+  my $nUrl = @URL;
+  my $nDestination = @destination;
+
+  if ($nDestination > $nUrl) {
+    warn("More file destinations than URLs given! Will use the first ones only\n");
+  } elsif ($nDestination > 1 && $nUrl > $nDestination) {
+    warn("More URLs than destinations given. STOP!\n");
+    return(-2);
+  } elsif ($nDestination == 1 && $nUrl > 1) {
+    if (! -d $destination[0]) {
+      $success = 0;
+      make_path($destination[0]) and $success = 1;
+      if (!$success) {
+        warn("Cannot create directory \"$destination[0]\": $!\n");
+        return(-4);
+      }
+    }
+    my $dir = $destination[0];
+    for (my $i=0; $i < $nUrl; $i++) {
+      $destination[$i] = $dir."/".basename($URL[$i]);
+    }
+  } elsif ($nDestination == 1 && $nUrl == 1) {
+    if (-d $destination[0]) {
+      $destination[0] = $destination[0]."/".basename($URL[0]);
+    } elsif (! -f $destination[0]) {
+      make_path($destination[0]) and $success = 1;
+      if (!$success) {
+        warn("Cannot create directory \"$destination[0]\": $!\n");
+        return(-4);
+      }
+      $destination[0] = $destination[0]."/".basename($URL[0]);
+    }
+  }
+  
+  # what happens if the destination list contains none existing paths?!
+
+  # adjusted from http://stackoverflow.com/questions/6813726/continue-getting-a-partially-downloaded-file
+  my $ua = LWP::UserAgent->new();
+
+  for (my $i=0; $i < $nUrl; $i++) {
+    unlink($destination[$i]) if ($forceReload && -f $destination[$i]);
+    $success = 1;
+    open(my $fh, '>>:raw', $destination[$i]) or $success = 0;
+    if (!$success) {
+      warn("Cannot open \"$destination[$i]\": $!\n");
+      return(-8);
+    }
+    my $bytes = -s $destination[$i];
+    my $res;
+    if ( $bytes && ! $forceReload) {
+      print "resume $URL[$i] -> $destination[$i] ($bytes) " if ($verbose);
+      $res = $ua->get(
+        $URL[$i],
+        'Range' => "bytes=$bytes-",
+        ':content_cb' => sub { my ( $chunk ) = @_; print $fh $chunk; }
+      );
+    } else {
+      print "$URL[$i] -> $destination[$i] " if ($verbose);
+      $res = $ua->get(
+        $URL[$i],
+        ':content_cb' => sub { my ( $chunk ) = @_; print $fh $chunk; }
+      );
+    }
+    close $fh;
+
+    my $status = $res->status_line;
+    if ( $status =~ /^(200|206|416)/ ) {
+      print "OK\n" if ($verbose && $status =~ /^20[06]/);
+      print "already complete\n" if ($verbose && $status =~ /^416/);
+    } else {
+      print "DEBUG: $status what happend?";
+    }
+  }
+  return(0);
+}
+
+########################################################################
+### Exported wrapper functions
 
 sub loadCache() {
   my $retval = setCache(0);
@@ -424,14 +572,12 @@ sub refreshCache() {
   return setCache(2);
 }
 
-### END: Functions to set up and refresh the cached data
+### END
 ########################################################################
-
 
 1;
 
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
@@ -451,17 +597,33 @@ ModisDownload - Perl extension for downloading MODIS satellite data
   loadCache();
 
   # print available versions of a certain product
-  print "$_\n" foreach (getVersions("MOD17A2"));
+  print "Versions of MOD17A2:";
+  print " $_" foreach (getVersions("MOD17A2"));
+  print "\n";
 
   # is the data in one file for the whole world?
-  print isGlobal("MOD17A3.005");
-  print "$_\n";
-  print isGlobal("MCD12C1.051");
-  print "$_\n";
+  print "nonexisting: ".isGlobal("nonexisting")."\n"; # this does not exist
+  print "MOD17A2.005: ".isGlobal("MOD17A2.005")."\n";
+  print "MCD12C1.051: ".isGlobal("MCD12C1.051")."\n";
 
-  # print a list of URLs
-  print "$_\n" foreach (getURLs("MOD17A3", ["2000.01.01", "2003.01.01"], [29, 30], [12]));
-  print "$_\n" foreach (getURLs("MCD12C1", "051", ["2005.01.01", "2006.01.01"]));
+  # print a list of URLs (can be used for wget)
+  my @URLs1 = getURLs("MOD17A2", ["2002.01.01", "2002.01.10"], [29, 30], [12]);
+  print "$_\n" foreach (@URLs1);
+  my @URLs2 = getURLs("MCD12C1", "051", ["2005.01.01", "2005.01.01"]);
+  print "$_\n" foreach (@URLs2);
+
+  # download the list to the current directory
+  modisDownload(\@URLs1);
+  system("mv MOD17A2.A2002001.h29v12.005.2007114184339.hdf MOD17A2.A2002001.h29v12.005.2007114184339.hdf.bak");
+  system("head -c 1051980 MOD17A2.A2002001.h29v12.005.2007114184339.hdf.bak > MOD17A2.A2002001.h29v12.005.2007114184339.hdf");
+  # output some info
+  modisDownload(\@URLs1, ".", 1);
+
+  # download the list and be verbose
+  # LARGE FILE! (1.3 GB)
+  modisDownload(\@URLs2, ".", 1);
+  # download the list to the current directory and force a reload of already existing files
+  modisDownload(\@URLs2, ".", 1, 1);
 
 =head1 DESCRIPTION
 
@@ -581,6 +743,23 @@ of Gigabytes to be downloaded.
 
 For examples see the SYNOPSIS section.
 
+=head2 modisDownload
+
+=for downloads file(s)
+
+=for usage
+  modisDownload({\@,$}URLs, [{\@,$}destination], [$verbose], [$forceReload]);
+
+continuous download of a list (as array reference) or a single file to a list
+(as array reference) of destination files or a destination directory. If a third
+argument is supplied it will be printed, what is downloaded and if a forth 
+argument is supplied (no matter what) allready existing files will be 
+overwritten. Otherwise they will be appended with the offset of the already
+existing file length.
+
+ATTENTION: Not yet checked for all possible stupid combinations of URLs
+and destination atomic or array combinations.
+
 =head1 TODO
 
 =over 4
@@ -594,7 +773,11 @@ if something unforseen happens. This needs to be improved.
 
 =item Use Carp where useful
 
+=item make_path dies if the path can not be created, catch it or use something else 
+for error handling
 
+=item rewrite setCache to store the data in memory only instead of writing the cached
+datato disk
 
 =back
 
